357c28254ae910ec5c679dfafc8352895d11ef26
Steven Choez <stevenchoez2000@gmail.com>
2021-08-26 19:55:08 -0500

    Corrección Customer

--- src/test/java/com/espol/rentalsystemtest/CustomerTest.java
+++ src/test/java/com/espol/rentalsystemtest/CustomerTest.java
@@ -1,12 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.espol.rentalsystemtest;
 
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -16,30 +15,28 @@ public class CustomerTest {
     
     public CustomerTest() {
     }
-
-    @org.junit.jupiter.api.BeforeAll
-    public static void setUpClass() throws Exception {
-    }
-
-    @org.junit.jupiter.api.AfterAll
-    public static void tearDownClass() throws Exception {
-    }
-
-    @org.junit.jupiter.api.BeforeEach
-    public void setUp() throws Exception {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    public void tearDown() throws Exception {
+    
+    @BeforeClass
+    public static void setUpClass() {
     }
     
+    @AfterClass
+    public static void tearDownClass() {
+    }
     
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
     /**
      * Test of addMovieRental method, of class Customer.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testAddMovieRental() {
-        System.out.println("addMovieRental");
         MovieRental arg = new MovieRental(new Movie("Erwin", Movie.CHILDRENS),4);
         Customer instance = new Customer("Erwin");
         instance.addMovieRental(arg);
@@ -50,9 +47,8 @@ public class CustomerTest {
     /**
      * Test of addVideoGameRental method, of class Customer.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testAddVideoGameRental() {
-        System.out.println("addVideoGameRental");
         VideoGameRental arg = new VideoGameRental(new Ps3Game("Emilio"), 12, true);
         Customer instance = new Customer("Emilio");
         instance.addVideoGameRental(arg);
@@ -64,15 +60,13 @@ public class CustomerTest {
     /**
      * Test of statement method, of class Customer.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testStatement() {
         System.out.println("statement");
         Customer instance = new Customer("Sasha");
         String expResult = instance.statement();
         String result = instance.statement();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        
     }
     
 }
